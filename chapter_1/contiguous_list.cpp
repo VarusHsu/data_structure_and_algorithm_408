6c1586bf50b36542fc90520a73b0ba5c430b45a3
XuShuo <xushuo@xiaohongshu.net>
2024-10-21 23:18:14 +0800

    update

--- chapter_1/contiguous_list.cpp
+++ chapter_1/contiguous_list.cpp
@@ -1,136 +1,183 @@
-#include<iostream>
+#include <iostream>
 #include "contiguous_list.h"
 using namespace std;
 
 template <typename T>
-Contiguous<T>::Contiguous(){
+Contiguous<T>::Contiguous()
+{
     this->capacity = INITIAL_CAPACITY;
     this->size = 0;
     this->header = new T[INITIAL_CAPACITY];
 }
 
 template <typename T>
-Contiguous<T>::~Contiguous(){
-
+Contiguous<T>::~Contiguous()
+{
 }
 
 template <typename T>
-bool Contiguous<T>::is_empty(){
+bool Contiguous<T>::is_empty()
+{
     return this->size == 0;
 }
 
 template <typename T>
-T* Contiguous<T>::pop_back(){
-    if (this->size == 0 ){
+T *Contiguous<T>::pop_back()
+{
+    if (this->size == 0)
+    {
         return nullptr;
     }
     return &this->header[--this->size];
 }
 
 template <typename T>
-void Contiguous<T>::clear(){
-    while(this->size>0){
+void Contiguous<T>::clear()
+{
+    while (this->size > 0)
+    {
         this->pop_back();
     }
     return;
 }
 
 template <typename T>
-void Contiguous<T>::push_back(T value){
-    if (this->capacity==this->size){
+void Contiguous<T>::push_back(T value)
+{
+    if (this->capacity == this->size)
+    {
         this->grow();
     }
     this->header[this->size++] = value;
 }
 
-
 template <typename T>
-void Contiguous<T>::grow(){
-    if (this->capacity == 0) {
+void Contiguous<T>::grow()
+{
+    if (this->capacity == 0)
+    {
         this->capacity = 10;
-    }else if (this->capacity <=256) {
-        this->capacity *= 2;        
-    }else {
-        this->capacity = (this->capacity + 3 * THRESHOLD) >>2;
     }
-    T* new_header = new T[this->capacity];
-    for (int i =0; i < this->length(); i ++){
-        new_header[i] = this-> header[i];
+    else if (this->capacity <= 256)
+    {
+        this->capacity *= 2;
+    }
+    else
+    {
+        this->capacity = (this->capacity + 3 * THRESHOLD) >> 2;
+    }
+    T *new_header = new T[this->capacity];
+    for (int i = 0; i < this->length(); i++)
+    {
+        new_header[i] = this->header[i];
     }
     delete this->header;
-    this-> header = new_header;
+    this->header = new_header;
 }
 
-template<typename T>
-int Contiguous<T>::length(){
+template <typename T>
+int Contiguous<T>::length()
+{
     return this->size;
 }
 
-template<typename T>
-T* Contiguous<T>::index_at(int index){
-    if (index >= this->size){
+template <typename T>
+T *Contiguous<T>::index_at(int index)
+{
+    if (index >= this->size)
+    {
         return nullptr;
     }
 
     return &this->header[index];
 }
 
-template<typename T>
-void Contiguous<T>::insert_at(int index, T value){
-    while(index >= this->capacity){
+template <typename T>
+void Contiguous<T>::insert_at(int index, T value)
+{
+    while (index >= this->capacity)
+    {
         this->grow();
     }
 
-    for(int i = this->size; i >= index; i--){
-        this->header[i] = this->header[i-1];
+    for (int i = this->size; i >= index; i--)
+    {
+        this->header[i] = this->header[i - 1];
     }
 
-    for (int i = this->size; i<index; i++ ){
-        T v =0;
+    for (int i = this->size; i < index; i++)
+    {
+        T v = 0;
         this->header[i] = v;
-        this -> size ++;
+        this->size++;
     }
 
-    this -> header[index] = value;
-    this -> size ++;
-
+    this->header[index] = value;
+    this->size++;
 }
 
-template<typename T>
-T* Contiguous<T>::delete_at(int index){
-    return nullptr;
+template <typename T>
+T *Contiguous<T>::delete_at(int index)
+{
+    if (this->size<= index){
+        return nullptr;    
+    }
+
+    T *value = this->header[index];
+
+    for (int i = index; i< this->size-1; i++){
+        this->header[i] = this-> header[i + 1];
+    }
+
+    this -> size --;
+    return &value;
 }
 
-template<typename T>
-void Contiguous<T>::print(){
-    if (this->size == 0) {
-        cout << "[]"<< endl;
-    } else {
-        cout<<"[";
-        for (int i=0; i < this-> size; i++){
-            cout<<*(this->index_at(i));
-            if (i != this->size-1){
-                cout<< " ";
-                cout<< ",";
-            }else {
-                cout<< "]"<<endl;
+template <typename T>
+void Contiguous<T>::print()
+{
+    if (this->size == 0)
+    {
+        cout << "[]" << endl;
+    }
+    else
+    {
+        cout << "[";
+        for (int i = 0; i < this->size; i++)
+        {
+            cout << *(this->index_at(i));
+            if (i != this->size - 1)
+            {
+                cout << " ";
+                cout << ",";
+            }
+            else
+            {
+                cout << "]" << endl;
             }
         }
     }
 
-    cout<< "The length of contiguous is "<<this->size<<";"<<endl;
-    cout<< "The capacity of contiguous is "<<this->capacity<<";"<<endl;
+    cout << "The length of contiguous is " << this->size << ";" << endl;
+    cout << "The capacity of contiguous is " << this->capacity << ";" << endl;
 }
 
-
-int main(){
+int main()
+{
     Contiguous<int> c;
-    for (int i = 0; i < 30; i ++) {
+    for (int i = 0; i < 30; i++)
+    {
         c.push_back(i);
     }
 
     c.insert_at(2, 100);
-        c.insert_at(50, 200);
+    c.insert_at(50, 200);
+    c.print();
+
+    c.delete_at(2);
+    c.print();
+
+    c.delete_at(49);
     c.print();
 
 }
